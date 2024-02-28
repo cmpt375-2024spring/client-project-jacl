3df4faf2f3b737103d0bd1cef8f1a644bc62cace
Arogya <109647530+uparogya@users.noreply.github.com>
2024-02-27 20:10:39 -0700

    arogya: navbar first wireframe copy completed

--- app/views.py
+++ app/views.py
@@ -2,15 +2,29 @@ from django.shortcuts import render
 
 # Create your views here.
 pages = {
-    '/': 'Home',
-    'about': 'About',
-    'events': 'Events',
-    'membership': 'Membership',
-    'programs': 'Programs',
+    'About Us': {
+        'Mission & Vision': 'mission',
+        'Board Members': 'board',
+        'Activism': 'activism',
+        'Cultural Programming': 'cultural_programming',
+    },
+    'Get Involved': {
+        'Join SLC JACL': 'join',
+        'Volunteer': 'volunteer',
+        'Events': 'events',
+    },
+    'Resources': {
+        'JACL Scholarships': 'scholarships',
+        'Japanese Cultural Organizations': 'orgs/japanese',
+        'Religious Oragnizations': 'orgs/religious',
+        'AANHPI Organizations': 'orgs/aanhpi',
+        'LGBTQIA+ Organizations': 'orgs/lgbtqiap'
+    },
+    'Contact': ''
 }
 
 context = { 'pages' : pages }
 
 def index(request):
-    context['active'] = '/'
+    context['active'] = ''
     return render(request, 'index.html', context)
