ab7168b2f5b5d19c0d629eeba676e9d2734a2c9f
Zac Durham <43076672+PapaZ810@users.noreply.github.com>
2024-02-28 13:45:15 -0700

    wireframe of urls

--- app/urls.py
+++ app/urls.py
@@ -3,4 +3,17 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('mission/', views.mission, name='mission'),
+    path('activism/', views.activism, name='activism'),
+    path('cultural/', views.cultural, name='cultural'),
+    path('join/', views.join, name='join'),
+    path('volunteer/', views.volunteer, name='volunteer'),
+    path('events/', views.events, name='events'),
+    path('scholarships/', views.scholarships, name='scholarships'),
+    path('orgs/', views.orgs, name='orgs'),
+    path('orgs/japanese/'),
+    path('orgs/religious/'),
+    path('orgs/aanhpi/'),
+    path('orgs/lgbtqiap/'),
+    path('contact/', views.contact, name='contact')
 ]
