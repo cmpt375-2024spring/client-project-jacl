abd53b33b08133ea1604b2b335227b607bc4a7c3
Zac Durham <43076672+PapaZ810@users.noreply.github.com>
2024-03-04 13:31:31 -0700

    refactor urls

--- app/urls.py
+++ app/urls.py
@@ -4,12 +4,12 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('mission/', views.mission, name='mission'),
-    path('activism/', views.activism, name='activism'),
-    path('cultural/', views.cultural, name='cultural'),
+    path('board/', views.board, name='board'),
+    path('history/', views.history, name='history'),
+    path('events/', views.events, name='events'),
     path('join/', views.join, name='join'),
     path('volunteer/', views.volunteer, name='volunteer'),
-    path('events/', views.events, name='events'),
     path('scholarships/', views.scholarships, name='scholarships'),
-    path('orgs/', views.orgs, name='orgs'),
-    path('contact/', views.contact, name='contact')
+    path('affiliates/', views.affiliates, name='orgs'),
+    path('contact/', views.contact, name='contact'),
 ]
