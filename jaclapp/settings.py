5571716b4d00f26cd688e3459e89ef7a85fc4041
Zac Durham <43076672+PapaZ810@users.noreply.github.com>
2024-03-04 12:33:23 -0700

    timezone and dir change

--- jaclapp/settings.py
+++ jaclapp/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'jaclapp.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'app/templates/admin'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -107,7 +107,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'MST'
 
 USE_I18N = True
 
