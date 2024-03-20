b0d2c16669aca081e920c78b493a1485996ebc6b
Zac Durham <43076672+PapaZ810@users.noreply.github.com>
2024-03-20 12:55:47 -0600

    board member admin addon

--- app/models.py
+++ app/models.py
@@ -1,2 +1,13 @@
+from django.contrib.auth.models import User
 from django.db import models
 
+
+class BoardMember(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    firstname = models.CharField(max_length=50)
+    lastname = models.CharField(max_length=50)
+    bio = models.TextField()
+    image = models.ImageField()
+
+    def __str__(self):
+        return self.firstname + " " + self.lastname
