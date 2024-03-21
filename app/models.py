ee067f49f5c1d41e15971b96cdaf48b0e46652b8
anthony-mackay-4715 <jam0111@westminsteru.edu>
2024-03-21 14:51:07 -0600

    Added model for home page images

--- app/models.py
+++ app/models.py
@@ -1,2 +1,9 @@
 from django.db import models
 
+
+class HomePageImage(models.Model):
+    title = models.CharField(max_length=50)
+    image = models.ImageField(upload_to='../app/static/user_upload/homePage')
+
+
+
