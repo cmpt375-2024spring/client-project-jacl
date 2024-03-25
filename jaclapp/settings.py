170ccee3078f63cc9296f71271e5494131f1d93b
Zac Durham <43076672+PapaZ810@users.noreply.github.com>
2024-03-25 12:35:17 -0600

    admin issues

--- jaclapp/settings.py
+++ jaclapp/settings.py
@@ -31,13 +31,13 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
+    "app.apps.AppConfig",
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    "app.apps.AppConfig",
     'django_extensions'
 ]
 
@@ -108,7 +108,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'MST'
+TIME_ZONE = 'MDT'
 
 USE_I18N = True
 
